1f37861b8c72fa6791f88e6ac40d909ce6cfb96f
Valentin Stoykov <valentin_vs@hotmail.com>
2013-05-13 11:42:58 +0300

    Merge branch 'master' of https://github.com/valentinvs/BaloonsPop
    Conflicts:
    	Refactored/Demo/Demo.cs

---
+++ Refactored/Demo/Demo.cs.BACKUP.5384.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yo yo");
+            Console.WriteLine("It's Marto !");
+<<<<<<< HEAD
+            Console.WriteLine("It's Marto ! Second Edition");
+=======
+            Console.WriteLine("Edin red :)");
+>>>>>>> 85ffbadbb1bf56bbcf0112613427291db3c5d67e
+        }
+    }
+}
---
+++ Refactored/Demo/Demo.cs.REMOTE.5384.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yo yo");
+            Console.WriteLine("It's Marto !");
+            Console.WriteLine("Edin red :)");
+        }
+    }
+}
