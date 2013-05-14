ab32ada298dca464b5d7713659fc52619d622424
NoMoree <i9q19@abv.bg>
2013-05-14 20:15:00 +0300

    Added PrinterField to Printer

--- Refactored/BaloonsPop/Printer.cs
+++ Refactored/BaloonsPop/Printer.cs
@@ -8,5 +8,27 @@ namespace BaloonsPop
 {
     class Printer
     {
+        void PrintField(Balloon[,] balloons)
+        {
+            // gameField.cs => ToString()
+            Console.WriteLine("    0 1 2 3 4 5 6 7 8 9");
+            Console.WriteLine("   ---------------------");
+
+            for (int row = 0; row < balloons.GetLength(0); row++)
+            {
+                Console.Write(row + " | ");
+
+                for (int column = 0; column < balloons.GetLength(1); column++)
+                {
+                    Console.ForegroundColor = MatchColor(balloons[row, column].Color);
+                    Console.Write(balloons[row, column].GetVisualisation() + " ");
+                }
+
+                Console.Write("| ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("   ---------------------");
+        }
     }
 }
