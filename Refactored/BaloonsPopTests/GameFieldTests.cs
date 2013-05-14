e003d9b00c75c5716f9191da15147a6ec029a504
Valentin Stoykov <valentin_vs@hotmail.com>
2013-05-14 21:33:32 +0300

    TestsUploaded

--- Refactored/BaloonsPopTests/GameFieldTests.cs
+++ Refactored/BaloonsPopTests/GameFieldTests.cs
@@ -7,13 +7,42 @@ namespace BaloonsPopTests
     [TestClass]
     public class GameFieldTests
     {
+        // Example test
         [TestMethod]
-        public void GameFieldInitializeMethod()
+        // [ExpectedException(typeof(ArgumentException))]
+        public void GameFieldWidthTest()
         {
             GameField gameField = new GameField();
+            int expectedFieldWidth = 10;
+            int actualFieldWidth = gameField.GameFieldWidth;
 
+            Assert.AreEqual(expectedFieldWidth, actualFieldWidth);
+            Assert.IsTrue(expectedFieldWidth == actualFieldWidth);
+        }
 
-            Assert.AreSame(gameField, gameField);
+        // Example test
+        [TestMethod]
+        // [ExpectedException(typeof(ArgumentException))]
+        public void GameFieldInitializeMethod()
+        {
+            GameField gameField1 = new GameField();
+            GameField gameField2 = new GameField();
+
+            int expected = gameField1.GameFieldHeight * gameField1.GameFieldWidth;
+            int actual = 0;
+
+            for (int row = 0; row < gameField1.BalloonsMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < gameField1.BalloonsMatrix.GetLength(1); col++)
+                {
+                    if (gameField1.BalloonsMatrix[row, col] == gameField2.BalloonsMatrix[row, col])
+                    {
+                        actual++;
+                    }
+                }
+            }
+
+            Assert.IsTrue(expected > actual);
         }
     }
 }
