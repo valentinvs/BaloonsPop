1e56467409a388cabc75505aa081d8b0cfe6caba
NoMoree <i9q19@abv.bg>
2013-05-14 20:16:55 +0300

    Added PrintField to Printer

--- Refactored/BaloonsPop/Printer.cs
+++ Refactored/BaloonsPop/Printer.cs
@@ -8,9 +8,12 @@ namespace BaloonsPop
 {
     class Printer
     {
+        /// <summary>
+        /// Print the Gamefield on the console
+        /// </summary>
+        /// <param name="balloons"></param>
         void PrintField(Balloon[,] balloons)
         {
-            // gameField.cs => ToString()
             Console.WriteLine("    0 1 2 3 4 5 6 7 8 9");
             Console.WriteLine("   ---------------------");
 
