f026a98ccccb52b7f825c6fc9ab919de1bdf5e39
Stefan Vitanov <stefan_1984sf@abv.bg>
2013-05-16 19:37:49 +0300

    ConsolePrinterTests added

---
+++ Refactored/BaloonsPopTests/ConsolePrinterTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BaloonsPop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaloonsPopTests
+{
+    [TestClass]
+    public class ConsolePrinterTests
+    {
+        [TestMethod]
+        public void MassageTest()
+        {
+            string input = "Text test";
+            var currentConsoleOut = Console.Out;
+
+            ConsolePrinter.Message(input);
+
+            using (var consoleOutput = new ConsoleOutput())
+            {
+                ConsolePrinter.Message(input);
+
+                Assert.AreEqual(input, consoleOutput.GetOuput());
+            }
+
+            Assert.AreEqual(currentConsoleOut, Console.Out);
+        }
+    }
+}
